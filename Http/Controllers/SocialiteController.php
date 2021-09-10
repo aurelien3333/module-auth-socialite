6ea74f849bd0339a46ec625d6205dac078e24ba8
aurelien3333 <aurelienverdeau@gmail.com>
2021-09-10 19:49:03 +0200

    fix redirect

--- Http/Controllers/SocialiteController.php
+++ Http/Controllers/SocialiteController.php
@@ -8,25 +8,30 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
 use Laravel\Socialite\Facades\Socialite;
+use Modules\BaseCore\Contracts\Repositories\EmailRepositoryContract;
 use Modules\BaseCore\Contracts\Repositories\PersonneRepositoryContract;
 use Modules\BaseCore\Contracts\Repositories\UserRepositoryContract;
 
 class SocialiteController extends Controller
 {
-    public function checkOrCreateUser(UserRepositoryContract $repUser, PersonneRepositoryContract $repPersonne): Redirector|Application|RedirectResponse
+    public function checkOrCreateUser(UserRepositoryContract $repUser, PersonneRepositoryContract $repPersonne, EmailRepositoryContract $repEmail): Redirector|Application|RedirectResponse
     {
         $userGoogle = Socialite::driver('google')->user();
 
         $user = $repUser->fetchByEmail($userGoogle->email);
 
         if (!is_object($user)) {
-            $personne = $repPersonne->createPersonneForRegister($userGoogle->user['given_name'], $userGoogle->email);
+
+            $personne = $repPersonne->createForRegister($userGoogle->user['given_name']);
+            $email = $repEmail->create( $userGoogle->email);
+            $repPersonne->makeRelation($personne->emails(), $email);
+
             $user = $repUser->create($personne, config('authsocialite.role_default'), $userGoogle->id);
         }
 
         Auth::loginUsingId($user->id, true);
 
-        return redirect(route(config('basecore.route_default')));
+        return redirect()->route(config('basecore.route_default'));
 
     }
 }
